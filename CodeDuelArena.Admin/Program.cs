e9b3406379b4196f20f7055807d0fb4ae7f9929b
Kraevoi <georgijkrai1@gmail.com>
2026-04-15 17:08:11 +0300

    Final Dockerfile fix

--- CodeDuelArena.Admin/Program.cs
+++ CodeDuelArena.Admin/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем DbContext
 var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connStr));
@@ -14,9 +13,28 @@ builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Автоматически создаём базу данных если её нет
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/Admin/Login");
+    return Task.CompletedTask;
+});
+
+app.Run();
