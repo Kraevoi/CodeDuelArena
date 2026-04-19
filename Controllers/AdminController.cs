7af199de7918cfde3917b8f92a4bed5233a3f352
Kraevoi <georgijkrai1@gmail.com>
2026-04-19 17:13:53 +0300

    Add missing using for QuestModel

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CodeDuelArena.Data;
+using CodeDuelArena.Models; 
+using Newtonsoft.Json;
 
 namespace CodeDuelArena.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly AppDbContext _db;
+        
+        public AdminController(AppDbContext db)
+        {
+            _db = db;
+        }
+        
         [HttpGet]
         public IActionResult Login()
         {
@@ -11,11 +22,15 @@ namespace CodeDuelArena.Controllers
         }
         
         [HttpPost]
-        public IActionResult Login(string password)
+        public IActionResult Login(string password, bool rememberMe)
         {
             if (password == "admin123")
             {
-                Response.Cookies.Append("admin_auth", "true");
+                Response.Cookies.Append("admin_auth", "true", new CookieOptions
+                {
+                    HttpOnly = true,
+                    Expires = rememberMe ? DateTime.Now.AddDays(30) : DateTime.Now.AddHours(1)
+                });
                 return RedirectToAction("Dashboard");
             }
             ViewBag.Error = "Wrong password";
@@ -25,11 +40,9 @@ namespace CodeDuelArena.Controllers
         [HttpGet]
         public IActionResult Dashboard()
         {
-            if (Request.Cookies["admin_auth"] == "true")
-            {
-                return View();
-            }
-            return RedirectToAction("Login");
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            return View();
         }
         
         [HttpGet]
@@ -37,6 +50,127 @@ namespace CodeDuelArena.Controllers
         {
             Response.Cookies.Delete("admin_auth");
             return RedirectToAction("Login");
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Users()
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            var users = await _db.Users.ToListAsync();
+            return View(users);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Quests()
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            var quests = DataStorage.GetQuests();
+            return View(quests);
+        }
+        
+        [HttpGet]
+        public IActionResult AddQuest()
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            return View(new QuestModel());
+        }
+        
+        [HttpPost]
+        public IActionResult AddQuest(QuestModel model)
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            
+            var quests = DataStorage.GetQuests();
+            model.Id = quests.Any() ? quests.Max(q => q.Id) + 1 : 1;
+            quests.Add(model);
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(quests, Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText("quests.json", json);
+            TempData["Message"] = "Квест добавлен!";
+            return RedirectToAction("Quests");
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Complaints()
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            var complaints = await _db.Complaints.OrderByDescending(c => c.CreatedAt).ToListAsync();
+            return View(complaints);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> MarkRead(int id)
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return Json(new { success = false });
+            
+            var complaint = await _db.Complaints.FindAsync(id);
+            if (complaint != null)
+            {
+                complaint.IsRead = true;
+                await _db.SaveChangesAsync();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> DeleteComplaint(int id)
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return Json(new { success = false });
+            
+            var complaint = await _db.Complaints.FindAsync(id);
+            if (complaint != null)
+            {
+                _db.Complaints.Remove(complaint);
+                await _db.SaveChangesAsync();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> ActivityLogs()
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            var logs = await _db.ActivityLogs.OrderByDescending(l => l.Timestamp).Take(200).ToListAsync();
+            return View(logs);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Impersonate(int id)
+        {
+            if (Request.Cookies["admin_auth"] != "true")
+                return RedirectToAction("Login");
+            
+            var user = await _db.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+            
+            Response.Cookies.Append("original_user", Request.Cookies["auth_user"] ?? "");
+            Response.Cookies.Append("auth_user", user.Username);
+            Response.Cookies.Delete("admin_auth");
+            
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpGet]
+        public IActionResult ReturnToAdmin()
+        {
+            var originalUser = Request.Cookies["original_user"];
+            if (!string.IsNullOrEmpty(originalUser))
+            {
+                Response.Cookies.Append("auth_user", originalUser);
+                Response.Cookies.Delete("original_user");
+            }
+            Response.Cookies.Append("admin_auth", "true");
+            return RedirectToAction("Dashboard");
         }
     }
 }
--- Models/UserDb.cs
+++ Models/UserDb.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
@@ -27,6 +26,8 @@ namespace CodeDuelArena.Models
         
         public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
         public DateTime LastLogin { get; set; } = DateTime.UtcNow;
+        
+        public bool IsAdmin { get; set; } = false;
         
         public List<string> CompletedQuests
         {
